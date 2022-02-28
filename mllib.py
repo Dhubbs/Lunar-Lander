7b011e711e7e1fefd40e28d40b94d092ef67ffee
Dylan Hubble <Hubbledylan@gmail.com>
2022-02-27 21:36:25 -0400

    Added Comments
    Added more comments

--- mllib.py
+++ mllib.py
@@ -1,5 +1,4 @@
-import random
-import math
+
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
@@ -8,73 +7,42 @@ import numpy as np
 import csv
 import pickle
 
-clf = 0
+#standard library I have developed to allow for ease of use of sklearn classifier
+#built on reading csv files with last column as the Target and all other columns as the predictors
+#saving and loading is done through pickling the model to allow for use else where
+#add scaling
 
-def fit(x,y,z,filename):
-    #change to x,y,z
-    clf = MLPClassifier(solver = 'lbfgs' , alpha = 1, hidden_layer_sizes = (x,y,z))
-    
-    print "Reading files..."
+clf = 0
+def fit(x,filename):
+    clf = MLPClassifier(solver = 'lbfgs' , alpha = 1, hidden_layer_sizes = x)
+    print("Reading files...")
     with open(filename, 'rb') as f:
         reader = csv.reader(f,quoting=csv.QUOTE_NONNUMERIC)
         vals = list(reader)
-    
-    
-    print "Cleaning Data..."
+    print("Cleaning Data...")
     y = [0 for i in range(len(vals))]
-    
+    #pop last column
     for i in range(len(vals)):
         y[i] = vals[i][len(vals[i])-1]
         del vals[i][len(vals[i])-1]
 
+    #allows Target to be non numeric
     lb = LabelEncoder()
     y = lb.fit_transform(y)
-    print "Splitting Data..."
+
+    print("Splitting Data...")
     vals = np.asarray(vals, dtype=np.float32)
 
-    
     X_train, X_test, y_train, y_test = train_test_split(vals,y,test_size=0.10)
-    print "Training..."
+    print("Training...")
     clf.fit(X_train,y_train)
-
     total = len(X_test)
     right = 0
-    rightconfi = 0
-
-    corr60 = 0
-    time60 = 1
-    corr75 = 0
-    time75 = 1
-    corr90 = 0
-    time90 = 1
-
-    print "Evaluating..."
+    print("Evaluating...")
     for i in range(len(X_test)):
-        #print clf.predict_proba([X_test[i]])
-        #print clf.predict([X_test[i]])
-        if(max(clf.predict_proba([X_test[i]])[0])>0.6):
-            time60 += 1
-            if(clf.predict([X_test[i]]) == y_test[i]):
-                corr60 += 1
-        if(max(clf.predict_proba([X_test[i]])[0])>0.75):
-            time75 += 1
-            if(clf.predict([X_test[i]]) == y_test[i]):
-                corr75 += 1
-
-        if(max(clf.predict_proba([X_test[i]])[0])>0.8):
-            time90 += 1
-            if(clf.predict([X_test[i]]) == y_test[i]):
-                corr90 += 1
-
         if clf.predict([X_test[i]]) == y_test[i]:
             right += 1
-
-
-    #print str(right/float(total)) + " out of " + str(float(total)) + " normal"
-    #print str(corr60/float(time60)) + " out of " + str(float(time60)) + " 60%"
-    #print str(corr75/float(time75)) + " out of " + str(float(time75)) + " 75%"
-    #print str(corr90/float(time90)) + " out of " + str(float(time90)) + " 80%"
-
+    #returns accuralcy + model
     return right/float(total),clf
 
 def save(model,filename):
@@ -82,15 +50,8 @@ def save(model,filename):
 def load(filename):
     return pickle.load(open(filename, 'rb'))
 def predict(model,vals):
-    #print "Splitting Data..."
-    #vals = np.asarray(vals, dtype=np.float32)
-    #return model.predict(vals.reshape(1,-1))
-    #for i in vals:
-    #print str(i) + " , " + str(model.predict([i]))
-    #print "Splitting Data..."
     vals = [vals]
     vals = np.asarray(vals, dtype=np.float32)
-    #vals = scale(vals,axis = 0,with_mean=False,with_std=True,copy=True)
     for i in vals:
         return model.predict([i])[0]
 
@@ -106,7 +67,7 @@ def predict(model,vals):
 
 
 
-#accuracy,model = fit(100,50,100,"out.txt")
+#accuracy,model = fit((100,50,100),"out.txt")
 #print(accuracy)
 
 
@@ -124,9 +85,6 @@ def predict(model,vals):
 #saves a model for later use
 #in1 = the neural net model
 #in2 = save file name, can be any extension
-
-
-
 #save(model,"first.sav")
 
 
--- neuralnet.py
+++ neuralnet.py
@@ -1,55 +1,53 @@
 from __future__ import division
 import gym
-import random
-import random
-import math
-from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import scale
-import numpy as np
-import csv
-import pickle
 from mllib import *
-
 
 #depends on a "seed" of data
 #continuously retrains a neural net and uses it to generate more data
-#
+#uses a neural net to build a greedy approach to the lunar lander problem and optimizes every step for max score
+#will need >1,000,000 data points to start seeing progress, and considerable compute
+#Chose to not render as I used a GCP vm that took ~5 hours for reasonable results
 
-
+#loads the enviroment
 env = gym.make('LunarLander-v2')
-
+#repeats the train test simulation 10 times, this value can be increased to allow for longer execution and more training
 for j in range(10):
-   
-    #builds a new 
-    accuracy,model = fit(10,10,4,"out.txt")
-    print("Accuracy:"+ str(accuracy))
-    
-    save(model,"first.sav")
-
-    f = open("out.txt",'a')
-    accuracy = 0
-    avg = 0
-    for i in range(50): 	
-    	    done = False
-	    s = env.reset()
-	    tot = 0
-	    while(not done):
-		#env.render()
-		before = s
-		#print(list(s))
-                #print(s)
-		reaction = predict(model,s)
-		s,score,done,a = env.step(reaction)
-		tot += score
-		if 1==1:
-			for j in before:
-				f.write(str(j)+",")
-			f.write(str(reaction) + "\n")
-		if done == True:
-			print i
-			print tot
-			avg += tot
-    print("AVG:" + str(avg/50.0))
-    f.close()
+    #retrains the model with the data stored in the out.txt file
+	#fir is a function from mllib.py
+	accuracy,model = fit((100,50,10),"out.txt")
+	#accuracy of the newly trained model
+	print("Accuracy:"+ str(accuracy))
+	#stores the currently trained model so that the program can be run continuosly and exited at any time with the most recent model saved
+	save(model,"first.sav")
+	#open our out.txt base data file to append more data
+	f = open("out.txt",'a')
+	avg = 0
+	#runs 50 simulated interations
+	for i in range(50):
+		#set up to run an individual simulation
+		done = False
+		s = env.reset()
+		tot = 0
+		#start the simulation
+		while(not done):
+			#saves the current state of the simulation so that it can be saved if a model
+			before = s
+			#predicts which rocket to fire based on the current state of the simulation
+			reaction = predict(model,s)
+			#applys the prediction and steps the simulation forward
+			s,score,done,a = env.step(reaction)
+			#keeps a rolling total of the current score for the simulation
+			tot += score
+			#if the action we took had a positive impact on our total score we want to record the enviroment + the action taken
+			if score>0.3:
+				#writes enviroment + reaction
+				for j in before:
+					f.write(str(j)+",")
+				f.write(str(reaction) + "\n")
+			#if we have completed simulation add total score to 50 simulation sum
+			if done == True:
+				avg += tot
+	#prints our avg score across the 50 simulaions
+	print("AVG:" + str(avg/50.0))
+	#closes out.txt so that it can be read from for retraining
+	f.close()
