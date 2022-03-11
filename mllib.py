a1b115b6c770d29466e16c4c8e9bb4086daa103f
Dylan Hubble <Hubbledylan@gmail.com>
2022-03-10 21:26:03 -0400

    updated comments
    Updated comments in mllib

--- mllib.py
+++ mllib.py
@@ -2,26 +2,27 @@
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import scale
 import numpy as np
 import csv
 import pickle
 
 #standard library I have developed to allow for ease of use of sklearn classifier
-#built on reading csv files with last column as the Target and all other columns as the predictors
+#built on reading csv files with last column as the Target and all other columns as the Predictors
 #saving and loading is done through pickling the model to allow for use else where
-#add scaling
 
 clf = 0
+#x = tuple describing size of hidden layers
+#filename = csv data containing training data
 def fit(x,filename):
+
     clf = MLPClassifier(solver = 'lbfgs' , alpha = 1, hidden_layer_sizes = x)
     print("Reading files...")
     with open(filename, 'rb') as f:
-        reader = csv.reader(f,quoting=csv.QUOTE_NONNUMERIC)
-        vals = list(reader)
+        vals = list(csv.reader(f,quoting=csv.QUOTE_NONNUMERIC))
     print("Cleaning Data...")
-    y = [0 for i in range(len(vals))]
+    y = [0] * len(vals)
     #pop last column
+    #creates a list y containing the Target + removes Target from rest of data
     for i in range(len(vals)):
         y[i] = vals[i][len(vals[i])-1]
         del vals[i][len(vals[i])-1]
@@ -31,8 +32,8 @@ def fit(x,filename):
     y = lb.fit_transform(y)
 
     print("Splitting Data...")
-    vals = np.asarray(vals, dtype=np.float32)
 
+    vals = np.asarray(vals, dtype=np.float32)
     X_train, X_test, y_train, y_test = train_test_split(vals,y,test_size=0.10)
     print("Training...")
     clf.fit(X_train,y_train)
@@ -42,7 +43,7 @@ def fit(x,filename):
     for i in range(len(X_test)):
         if clf.predict([X_test[i]]) == y_test[i]:
             right += 1
-    #returns accuralcy + model
+    #returns accuralcy,model object
     return right/float(total),clf
 
 def save(model,filename):
@@ -54,47 +55,3 @@ def predict(model,vals):
     vals = np.asarray(vals, dtype=np.float32)
     for i in vals:
         return model.predict([i])[0]
-
-
-#builds your model
-#in1 = first layer size
-#in2 = second layer size
-#in3 = third layer size
-#in4 = training data file
-#training data must have what you are predicting too as last column
-#out1 = accuracy of model
-#out2 = model
-
-
-
-#accuracy,model = fit((100,50,100),"out.txt")
-#print(accuracy)
-
-
-
-
-
-#predictis on Data
-#in1 = model object
-#in2 = file of data to predict on
-#predict data must be one row with (number of columns of training data - 1) columns
-#prints the data as well as the prediction
-#can be modified to return it
-#predict(model,"pre.csv")
-
-#saves a model for later use
-#in1 = the neural net model
-#in2 = save file name, can be any extension
-#save(model,"first.sav")
-
-
-
-#loads a model from file
-#in1 = location of a previously built model
-#out1 = returns a neural net object
-#load("first.sav")
-
-
-#pip install -U scikit-learn
-#pip install numpy
-
